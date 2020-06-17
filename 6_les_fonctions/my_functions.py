e944dfbd68e4b322c3321114fe5f5f4d38674f77
Dueymes <guillaume.dueymes@gmail.com>
2020-06-16 22:41:37 -0400

    first commit

---
+++ 6_les_fonctions/my_functions.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 13:56:30 2018
+
+@author: guillaume
+"""
+def addition(a, b):
+   """This program adds two
+   numbers and return the result"""
+   result = a + b
+   return result
+
+def substraction(a, b):
+   """This program subs two
+   numbers and return the result"""
+   result = a - b
+   return result
+
+def multiplication(a, b):
+   """This program multiplies two
+   numbers and return the result"""
+   result = a * b
+   return result
